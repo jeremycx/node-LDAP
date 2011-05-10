9e3564d20592f9ee591a4a83c7808ffb9208bbbe
Jeremy Childs <jeremyc@ssimicro.com>
2011-05-10 14:32:02 -0600

    Added tests

---
+++ tests/test1.js
@@ -0,0 +1,40 @@
+var LDAP = require('../LDAP');
+var cnx = new LDAP.Connection();
+
+cnx.maxconnectretries = 3;
+cnx.retrywait = 10;
+
+function test1() {
+    cnx.Open("ldap://localhost:43241", function(err) {
+        if (err) {
+            console.log("Got : "+ err + " [OK]")
+            test2();
+        } else {
+            throw(err);
+        }
+    });
+}
+
+function test2() {
+   cnx.Open("ldap://no.lookup:43241", function(err) {
+        if (err) {
+            console.log("Got : "+ err + " [OK]")            
+            test3();
+        } else {
+            throw(err);
+        }
+    });
+}
+
+function test3() {
+   cnx.Open("ldap://localhost:34524,ldap://no.lookup:43241", function(err) {
+        if (err) {
+            console.log("Got : "+ err + " [OK]");
+            cnx.close(); //OK to close, even though the connection is not open
+        } else {
+            throw(err);
+        }
+    });
+}
+
+test1();
---
+++ tests/test2.js
@@ -0,0 +1,6 @@
+var LDAP = require('../LDAP');
+var cnx = new LDAP.Connection();
+
+// these tests are all run against a live openLDAP database.
+// likely this is for module development only - not to be run automatically.
+
