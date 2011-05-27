1b07b4aa03d7df74fff7fbbb05e68dbd058a640a
Jeremy Childs <jeremyc@ssimicro.com>
2011-05-27 13:13:50 -0600

    SimpleBind() implemented. Now handles NULL callbacks.

--- LDAP.js
+++ LDAP.js
@@ -5,14 +5,18 @@ var Connection = function() {
     var binding = new ldapbinding.LDAPConnection();
     var self = this;
     var querytimeout = 5000;
+    var totalqueries = 0;
 
-    self.SetCallback(msgid, CB) {
+    self.SetCallback = function(msgid, CB) {
         if (msgid > 0) {
-            callbacks[msgid] = CB;
-            callbacks[msgid].tm = setTimeout(function() {
-                CB(msgid, -2);
-                delete callbacks[msgid];
-            }, querytimeout);
+            totalqueries++;
+            if (typeof(CB) == 'function') {
+                callbacks[msgid] = CB;
+                callbacks[msgid].tm = setTimeout(function() {
+                    CB(msgid, -2);
+                    delete callbacks[msgid];
+                }, querytimeout);
+            }
         }
         return msgid;
     }
@@ -23,6 +27,16 @@ var Connection = function() {
 
     self.Search = function(base, scope, filter, attrs, CB) {
         var msgid = binding.Search(base, scope, filter, attrs);
+        return self.SetCallback(msgid, CB);
+    }
+
+    self.SimpleBind = function(binddn, password, CB) {
+        var msgid;
+        if (arguments.length == 0) {
+            msgid = binding.SimpleBind();
+        } else {
+            msgid = binding.SimpleBind(binddn, password);
+        }
         return self.SetCallback(msgid, CB);
     }
 
@@ -37,16 +51,21 @@ var Connection = function() {
     }
 
     binding.addListener("searchresult", function(msgid, result, data) {
-        clearTimeout(callbacks[msgid].tm);
-        callbacks[msgid](msgid, result, data);
-        delete(callbacks[msgid]);
+        if (callbacks[msgid]) {
+            clearTimeout(callbacks[msgid].tm);
+            callbacks[msgid](msgid, result, data);
+            delete(callbacks[msgid]);
+        }
     });
 
     binding.addListener("result", function(msgid, result) {
-        clearTimeout(callbacks[msgid].tm);
-        callbacks[msgid](msgid, result);
-        delete(callbacks[msgid]);
+        if (callbacks[msgid]) {
+            clearTimeout(callbacks[msgid].tm);
+            callbacks[msgid](msgid, result);
+            delete(callbacks[msgid]);
+        }
     });
+
 }
 
 exports.Connection = Connection;
