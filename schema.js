a9fd5956da1465aae605ab694054c400107903ad
Jeremy Childs <jeremyc@ssimicro.com>
2012-03-20 09:40:32 -0600

    Implement custom everything via callback. much more sensible. Integration with testing framework.

--- schema.js
+++ schema.js
@@ -2,38 +2,150 @@ var fs = require('fs');
 
 var schema_raw = [];
 
-var schema;
+schema_raw = schema_raw.concat(JSON.parse(fs.readFileSync(__dirname + '/schema/default.schema')));
+schema_raw = schema_raw.concat(JSON.parse(fs.readFileSync(__dirname + '/schema/nonstandard.schema')));
 
-schema_raw = schema_raw.concat(JSON.parse(fs.readFileSync('schema/default.schema')));
-schema_raw = schema_raw.concat(JSON.parse(fs.readFileSync('schema/nonstandard.schema')));
+var ObjectClass = function(obj, lookup_fn) {
+    var must = {};
+    var may = {};
+    var all = {};
+    var attr;
 
-function initSchema() {
-    schema = {
-        objectclasses: {},
-        attributes: {}
-    }
-    schema_raw.forEach(function(item) {
-        if (item.type == 'ATTRIBUTE') {
-            item.name.forEach(function(name) {
-                schema.attributes[name] = item;
-            });
-        } else if (item.type == 'OBJECTCLASS') {
-            schema.objectclasses[item.name] = item;
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            this[i] = obj[i];
         }
-    });
+    }
+
+    if (obj.must) {
+        obj.must.forEach(function(attrname) {
+            attr = lookup_fn(attrname);
+            must[attrname] = attr;
+            all[attrname] = attr;
+        });
+    }
+    if (obj.may) {
+           obj.may.forEach(function(attrname) {
+            attr = lookup_fn(attrname);
+            may[attrname] = attr;
+            all[attrname] = attr;
+           });
+    }
+
+
+    // adds attributes that aren't already in the accum
+    function mergeAttributes(accum) {
+        for (var i in this.all) {
+            accum[i] = this.all[i];
+        }
+    }
+
+    this.all = all;
+    this.must = must;
+    this.may = may;
+    this.mergeAttributes = mergeAttributes;
 }
 
+var Attribute = function(obj) {
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            this[i] = obj[i];
+        }
+    }
+}
 
-module.exports = function(customschemafiles, customhelperfiles) {
-    var schema = schema;
+module.exports = function(opt) {
+    var schema;
+    var customschema = undefined;
 
-    // TODO: merge in custom files provided during instantiation
+    // dummy for init_attr;
+    var init_attr = function() {
+        return;
+    }
+
+    // dummy for init_ibj
+    var init_obj = function() {
+        return;
+    }
+    
+    if (!opt) opt = {};
+
+    if (typeof opt.customschema == 'string') {
+        customschema = [ opt.customschema ];
+    } else if (typeof opt.customschema == 'object') {
+        customschema = opt.customschema;
+    }
+
+    customschema.forEach(function(file) {
+        schema_raw = schema_raw.concat(JSON.parse(fs.readFileSync(file)));
+    });
+
+    if (typeof opt.init_attr == 'function') {
+        init_attr = opt.init_attr;
+    }
+    if (typeof opt.init_obj == 'function') {
+        init_obj = opt.init_obj;
+    }
 
     initSchema();
 
+    function initSchema() {
+        schema = {
+            objectclasses: {},
+            attributes: {}
+        }
+        schema_raw.forEach(function(item) {
+            if (item.type == 'ATTRIBUTE') {
+                item.name.forEach(function(name) {
+                    schema.attributes[name] = new Attribute(item);
+                    init_attr(schema.attributes[name]);
+                });
+            }
+        });
+        // attrs loaded. Second pass for ObjClass
+        schema_raw.forEach(function(item) {
+            var tmp;
+            if (item.type == 'OBJECTCLASS') {
+                schema.objectclasses[item.name] = new ObjectClass(item, getAttribute);
+                init_obj(schema.objectclasses[item.name]);
+            }
+        });
+    }
+
+    function getAttributesForRec(data) {
+        var res = [];
+
+        if (!data || !data.objectClass) {
+            return undefined;
+        }
+        data.objectClass.forEach(function(oc) {
+            var obj = getObjectClass(oc);
+            if (obj) {
+                if (obj.must) {
+                    res = res.concat(obj.must);
+                }
+                if (obj.may) {
+                    res = res.concat(obj.may);
+                }
+            }
+        });
+        return res;
+    }
+
+    function mergeHelper(helper) {
+        for (var i in helper.attrs) {
+            var attr = getAttribute(i);
+            if (attr) {
+                for (var j in helper.attrs[i]) {
+                    attr[j] = helper.attrs[i][j];
+                }
+            }
+        }
+    }
+
     function getObjectClass(name) {
         if (schema.objectclasses[name]) {
-            return schema.objectclasses
+            return schema.objectclasses[name];
         }
         return undefined;
     }
@@ -42,23 +154,34 @@ module.exports = function(customschemafiles, customhelperfiles) {
         if (schema.attributes[name]) {
             return schema.attributes[name];
         }
+        return undefined;
     }
 
-    function getAttributesForObjectClass(name) {
-        var obj;
-        var attrs = [];
+    function getUniqueAttributes(rec) {
+        var res = {};
 
-        if (obj = getObjectClass(name)) {
-            attrs = attrs.concat(obj.must);
-            attrs = attrs.concat(obj.may);
-            return atrts;
-        } else {
-            return undefined;
+        if (!rec) return undefined;
+        
+        for (var i in rec.objectClass) {
+            var oc = getObjectClass(rec.objectClass[i]);
+            if (oc) oc.mergeAttributes(res);
         }
+        return res;
+    }
+
+    function getAllAttributes() {
+        return schema.attributes;
+    }
+
+    function getAllObjectClasses() {
+        return schema.objectclasses;
     }
 
 
     this.getObjectClass = getObjectClass;
     this.getAttribute = getAttribute;
-    this.getAttributesForObjectClass = getAttributesForObjectClass;
+    this.getAttributesForRec = getAttributesForRec;
+    this.getUniqueAttributes = getUniqueAttributes;
+    this.getAllObjectClasses = getAllObjectClasses;
+    this.getAllAttributes = getAllAttributes;
 }
