18ce3e2c017d44701ef0c7074c9390952fcca2d1
Joshua Houghton <joshua.j.houghton@googlemail.com>
2020-03-04 13:25:29 +0000

    check if cacert path is of type string instead of not undefined

--- LDAPCnx.cc
+++ LDAPCnx.cc
@@ -62,7 +62,7 @@ void LDAPCnx::New(const Nan::FunctionCallbackInfo<Value>& info) {
     int debug           = info[5]->NumberValue();
     int verifycert      = info[6]->NumberValue();
     int referrals       = info[7]->NumberValue();
-    Nan::Utf8String        cacertfile(info[8]);
+    Nan::Utf8String       cacertfile(info[8]);
     int zero            = 0;
 
     ld->ldap_callback = (ldap_conncb *)malloc(sizeof(ldap_conncb));
@@ -77,7 +77,7 @@ void LDAPCnx::New(const Nan::FunctionCallbackInfo<Value>& info) {
 
     struct timeval ntimeout = { timeout/1000, (timeout%1000) * 1000 };
 
-    if (!info[8]->IsUndefined()) {
+    if (info[8]->IsString()) {
       ldap_set_option(ld->ld, LDAP_OPT_X_TLS_CACERTFILE, *cacertfile);
     }
 
